9a320ac7ace53c50892ee817fe0383096992047a
Latena15 <lmh2197@columbia.edu>
2015-05-04 17:58:43 -0400

    first init

---
+++ Final/js/main.js
@@ -0,0 +1,14 @@
+(function(){
+	
+	$('.answer').on('click', function(){
+		console.log("hello")
+		$(this).parent().addClass("answered")
+	});
+
+
+// on.answerdiv.click,
+// 	addaclassof.answeredtothatchoices.answer2-container
+
+
+
+}).call(this);
